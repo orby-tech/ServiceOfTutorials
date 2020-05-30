1d88578b6323f0e77a0ec6b772767f1c6179ebfc
orby-tech <tibode495@gmail.com>
2020-05-30 11:36:18 +0300

    multilanguage in footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,19 +1,20 @@
 import React, { Component }  from 'react';
+import { multilanguage, changeLanguage, loadLanguages} from "redux-multilanguage";
+import  { connect } from 'react-redux'
 
-
-class Footer extends Component{
+class PREFooter extends Component{
 
 
   render() {
-
+    const { strings, currentLanguageCode } = this.props;
     return(   
       <div className="footer">
-      <p className="footer__about"> Создал и поддерживает проект:</p>
-      <a className="footer__link" href="http://www.orby.site/about">  Бондаренко Тимур </a>
+      <p className="footer__about"> {strings["footerCreate"]}</p>
+      <a className="footer__link" href="http://www.orby.site/about"> {strings["footerAuthor"]} </a>
 
       </div>
     );
   }
 }
-
+const Footer = connect()(multilanguage(PREFooter));
 export default Footer;
