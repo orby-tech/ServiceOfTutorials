4d7759f640985796d427577a84b98dfaaedb8ec5
orby-tech <tibode495@gmail.com>
2020-06-10 10:41:37 +0300

    new admin panel

--- admin/src/NavBar.js
+++ admin/src/NavBar.js
@@ -112,7 +112,13 @@ class PRENavBar extends Component{
                         className="nav-link"
                         to="/CreateArticle"> {strings["navBarWrite"]}
                   </Link>
-                </li>    
+                </li>                    
+                <li className="nav-item" id="navBar">
+                  <Link onClick={this.toggleNavbar} 
+                        className="nav-link"
+                        to="/admin"> admin panel
+                  </Link>
+                </li> 
                 <li className="nav-item" id="navBar">
                   <Link onClick={this.toggleNavbar} 
                         className="nav-link"
